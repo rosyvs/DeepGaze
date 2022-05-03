7c71df4bc0aed82d91106c5d9057770b2e8d85b0
Rick Gentry <rige3027@colorado.edu>
2022-05-03 12:45:03 -0600

    adding sbatch script

---
+++ scripts/run_hyp_exp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=03:00:00
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=fix-hyp-exp
+#SBATCH --output=fix-hyp-exp.%j.out
+
+module purge
+module load python/3.8.0
+module load cuda/11.2
+
+# Run script
+cd /projects/$USER/eyemind
+pip install .
+python3 eyemind/experiments/cross_val_fixation.py --data_folderpath ../fixation --log_dirpath lightning_logs --experiment_logging_folder seqlen_hiddendim_testing --sequence_length 100 250 500 --gru_hidden_size 128 256 --gpus 1 --max_epochs 10 --log_every_n_steps 1000
