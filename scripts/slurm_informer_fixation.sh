68f25832ae125e1ce118ab80d17ddc4d19af8d3d
Rick Gentry <rige3027@colorado.edu>
2022-07-14 09:45:11 -0700

    informer fixation test

---
+++ scripts/slurm_informer_fixation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=04:00:00
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=informer-fix
+#SBATCH --cpus-per-task=4
+#SBATCH --output=informer-fix-exp.%j.out
+
+module load cuda/11.2
+module load cudnn/8.1_for_cuda_11.2
+
+# Run script
+source ~/.bashrc
+cd /projects/$USER/eyemind
+conda activate eyemind
+pip install .
+python3 eyemind/experiments/informer_experiment.py -c experiment_configs/informer_test_config.yml
