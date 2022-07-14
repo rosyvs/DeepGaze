73531a002384d7169c1a0668d30a340b074f3706
Rick Gentry <rige3027@colorado.edu>
2022-07-14 11:09:55 -0700

    log total loss

--- eyemind/models/encoder_decoder.py
+++ eyemind/models/encoder_decoder.py
@@ -317,6 +317,7 @@ class MultiTaskEncoderDecoder(VariableSequenceLengthEncoderDecoderModel):
             self.log(f"{step_type}_{task}_loss", task_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
             self.log(f"{step_type}_{task}_metric", task_metric, on_step=True, on_epoch=True, prog_bar=True, logger=True)
             total_loss += task_loss
+        self.log(f"{step_type}_loss", total_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return total_loss
 
     def configure_optimizers(self):
