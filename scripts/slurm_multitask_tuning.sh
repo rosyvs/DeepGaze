3ff18394f3be9d9e886e7838189002d303876f50
Rick Gentry <rige3027@colorado.edu>
2022-07-14 09:32:13 -0700

    slurm multitask script

---
+++ scripts/slurm_multitask_tuning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=04:00:00
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=multitask-tune
+#SBATCH --cpus-per-task=4
+#SBATCH --output=multitask-tune-exp.%j.out
+
+module load cuda/11.2
+module load cudnn/8.1_for_cuda_11.2
+
+# Run script
+source ~/.bashrc
+cd /projects/$USER/eyemind
+conda activate eyemind
+pip install .
+python3 eyemind/experiments/fixation_tuning.py -c experiment_configs/multitask_tuning_config.yml --gpus_per_trial 1 --exp_name multitask_tuning
