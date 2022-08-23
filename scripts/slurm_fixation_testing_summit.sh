3a5cb948de50961aa2dd5da2b6db89f87286ef1f
Rick Gentry <rige3027@colorado.edu>
2022-08-23 12:24:26 -0700

    testing just fixation id

---
+++ scripts/slurm_fixation_testing_summit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=fixation-test-summit
+#SBATCH --cpus-per-task=4
+#SBATCH --output=fixation-test-summit-exp.%j.out
+
+module load cuda/11.2
+module load cudnn/8.1_for_cuda_11.2
+
+# Run script
+source ~/.bashrc
+cd /projects/$USER/eyemind
+conda activate eyemind
+pip install .
+python3 eyemind/experiments/multitask_experiment.py fit -c experiment_configs/encdec_fixation_config.yml 
