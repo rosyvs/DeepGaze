93adfdfbdc8f30709563126f9a3de1638de841b5
Rick Gentry <rige3027@colorado.edu>
2022-08-24 14:14:11 -0700

    update pip install

--- scripts/slurm_multitask_testing_alpine.sh
+++ scripts/slurm_multitask_testing_alpine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=2:00:00
+#SBATCH --time=1:00:00
 #SBATCH --partition=aa100
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -10,11 +10,11 @@
 #SBATCH --output=multitask-test-alpine-exp.%j.out
 
 module load cuda/11.2
-module load cudnn/8.1_for_cuda_11.2
+module load cudnn/8.1
 
 # Run script
-source ~/.bashrc
+source /projects/$USER/.bashrc_alpine
 cd /projects/$USER/eyemind
 conda activate eyemind
-pip install .[cuda112]
+pip install .[cuda112] --use-feature=in-tree-build
 python3 eyemind/experiments/multitask_experiment.py fit -c experiment_configs/encdec_multitask_config.yml --trainer.max_epochs 5
