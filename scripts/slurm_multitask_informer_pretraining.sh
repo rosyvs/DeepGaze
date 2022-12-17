9e946ad0644c2e2886bcec4a97955ece65a7dde6
Rick Gentry <rige3027@colorado.edu>
2022-12-17 11:19:47 -0800

    alpine informer pretraining

---
+++ scripts/slurm_multitask_informer_pretraining.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --partition=aa100
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:3
+#SBATCH --job-name=multitask-informer-pretraining
+#SBATCH --cpus-per-task=4
+#SBATCH --output=multitask-informer-pretraining-exp.%j.out
+
+module load cudnn/8.1
+module load cuda/11.3
+
+
+# Run script
+source /projects/$USER/.bashrc_alpine
+cd /projects/$USER/eyemind
+conda activate pya100
+pip install .
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
+python3 eyemind/experiments/informer_experiment.py fit -c experiment_configs/inf_multitask_config.yml
