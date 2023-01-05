7eeef983376802dc295f859285b72fae4fae5f48
Rick Gentry <rige3027@colorado.edu>
2023-01-05 14:45:01 -0800

    trying to fix checkpoint path

--- eyemind/experiments/cli.py
+++ eyemind/experiments/cli.py
@@ -1,7 +1,7 @@
 from typing import Any
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities.cli import LightningCLI, LightningArgumentParser
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 from eyemind.trainer.loops import KFoldLoop
 
 
