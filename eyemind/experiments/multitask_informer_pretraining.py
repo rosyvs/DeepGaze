dcb24e15c75f4159285ae4c1ddc14fb2a13ba38a
rosyvs <rosy.southwell@gmail.com>
2023-04-08 21:26:38 -0600

    add cwd print

--- eyemind/experiments/multitask_informer_pretraining.py
+++ eyemind/experiments/multitask_informer_pretraining.py
@@ -2,9 +2,10 @@ from pytorch_lightning.utilities.cli import LightningCLI
 from eyemind.models.transformers import InformerEncoderDecoderModel, InformerEncoderFixationModel, InformerMultiTaskEncoderDecoder
 from eyemind.dataloading.informer_data import InformerDataModule
 from eyemind.experiments.cli import FoldsLightningCLI
-
+import os
 
 if __name__ == "__main__":
+    print(f'CWD: {os.getcwd()}')
     cli = FoldsLightningCLI(InformerMultiTaskEncoderDecoder, InformerDataModule, run=False, seed_everything_default=42)
     cli.datamodule.setup()
     if cli.config.num_folds != -1:
