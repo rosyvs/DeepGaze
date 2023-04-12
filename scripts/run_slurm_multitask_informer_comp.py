5e86d8cd98eb2cbeef4cdfb42c0450d52b59cc3e
rosyvs <rosy.southwell@gmail.com>
2023-04-11 19:08:52 -0600

    bug where list was put in a list

--- scripts/run_slurm_multitask_informer_comp.py
+++ scripts/run_slurm_multitask_informer_comp.py
@@ -19,7 +19,7 @@ def main(args):
             ckpt_path = str(next(ckpt_dirpath.glob('last*.ckpt')))
         else: # get most recent checkpoint
             files=ckpt_dirpath.glob('epoch*.ckpt')
-            latest_file = max([list(files)], key=lambda item: item.stat().st_ctime)
+            latest_file = max(list(files), key=lambda item: item.stat().st_ctime)
             ckpt_path = str(latest_file)    
         cmd = f"sbatch {args.slurm_script} {i} {args.seed} {ckpt_path} {args.label_col}"
         print(cmd)
